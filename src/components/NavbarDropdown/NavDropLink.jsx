2e3f27e1b65a5f821e37105d63a511271a4ed087
Gerard Almenara <ger.almenara@gmail.com>
2022-12-23 03:45:53 +0100

    fixed issues with mobile nav in landing

--- src/components/NavbarDropdown/NavDropLink.jsx
+++ src/components/NavbarDropdown/NavDropLink.jsx
@@ -1,10 +1,11 @@
+import { Link } from "react-router-dom";
 import "./navbar-dropdown.scss";
 
 const NavDropLink = (link) => {
   return (
-    <div className="navbar-dropdown-link">
-      <a href={link.href}>{link.name}</a>
-    </div>
+    <Link to={link.href} className="navbar-dropdown-link">
+      {link.name}
+    </Link>
   );
 };
 
